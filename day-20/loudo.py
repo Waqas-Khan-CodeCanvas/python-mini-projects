4a964adfeb5be42ea4473b8f7093cfa954babcaa
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-23 23:54:48 +0500

    lodo

---
+++ day-20/loudo.py
@@ -0,0 +1,16 @@
+import random
+
+num=random.randint(1,6)
+
+if num == 1:
+    print("*")
+elif num == 2:
+    print("* *")
+elif num == 3:
+    print("* * *")
+elif num == 4:
+    print("* * \n* * ")
+elif num == 5:
+    print("* * * \n*  * ")
+else:
+    print("* * * \n* * * ")
